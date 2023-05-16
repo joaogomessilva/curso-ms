a72f825817d03445b7e3b4a164d7d20ac9484921
joaogomessilva <joaogomes.silva@uol.com.br>
2023-05-16 09:50:50 -0300

    Hr-api-gateway

---
+++ hr-api-gateway-zuul/src/test/java/com/examplo/demo/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplo.demo.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
